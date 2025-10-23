cf559cf94b4def3556dc939a424ccf7d319543f2
DESKTOP-5SRV5A8\human <kimy9534@naver.com>
2025-10-23 10:28:32 +0900

    이미지 업로드 페이지 백엔드 수정+db연결오류 해결(application.properties 경로 문법 수정, controller crossOrigin 추가)

--- calopic/backend/src/main/java/fs/human/calopic/upload/controller/UploadController.java
+++ calopic/backend/src/main/java/fs/human/calopic/upload/controller/UploadController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/upload")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UploadController {
 
     private final UploadService uploadService;
--- calopic/backend/src/main/java/fs/human/calopic/upload/vo/UploadVO.java
+++ calopic/backend/src/main/java/fs/human/calopic/upload/vo/UploadVO.java
@@ -2,15 +2,31 @@ package fs.human.calopic.upload.vo;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class UploadVO {
-    private int foodId;
+//    private int foodId;
+//    private String foodName;
+//    private String foodWeight;
+//    private double foodKcal;
+//    private double foodCarbo;
+//    private double foodProtein;
+//    private double foodFat;
+//    private double foodSugar;
+//    private double foodNatrium;
+//
+//    private Integer foodWeightGram; //foodWeight 숫자만 파싱
+
+    private Long foodId;
     private String foodName;
-    private double foodWeight;
-    private double foodKcal;
-    private double foodCarbo;
-    private double foodProtein;
-    private double foodFat;
-    private double foodSugar;
-    private double foodNatrium;
+    private String foodWeight;          // 원본 "100g"
+    private Integer foodWeightGram;     // 숫자만 파싱해서 담을 곳
+
+    private BigDecimal foodKcal;
+    private BigDecimal foodCarbo;
+    private BigDecimal foodProtein;
+    private BigDecimal foodFat;
+    private BigDecimal foodSugar;
+    private BigDecimal foodNatrium;
 }
